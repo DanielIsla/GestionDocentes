ddc0dfc5d94a74b59ce822dfbc369f70320579ef
Daniel Isla Martinez <daniel.isla.martinez@gmail.com>
2025-12-02 13:59:46 +0000

    Subida proyecto SpringBoot creado con Spring Initializr

---
+++ src/test/java/com/example/GestionDocentes/GestionDocentesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GestionDocentes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDocentesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
